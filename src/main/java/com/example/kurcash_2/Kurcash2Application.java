a187fe580daf1f7f09f87959df786b73310f16d1
Artemii Usov <artemij.usov@yandex.ru>
2022-09-23 22:23:43 +0300

    Initial commit

---
+++ src/main/java/com/example/kurcash_2/Kurcash2Application.java
@@ -0,0 +1,13 @@
+package com.example.kurcash_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kurcash2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kurcash2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kurcash_2/Kurcash2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kurcash_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kurcash2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
